ff24f25f5f342329cf372345fcf7924f24788f88
Yuri Sousa <yuricavalcante1@gmail.com>
2019-05-19 13:46:23 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,9 +1,14 @@
 import { HttpHandler, HttpInterceptor, HttpEvent, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(public storage: StorageService) {
+
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
@@ -21,8 +26,18 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log("Erro detectado pelo interceptor: ");
             console.log(errorObj);
 
+            switch(errorObj) {
+                case 403:
+                this.handle403();
+                break;
+            }
+
             return Observable.throw(errorObj);
         }) as any;
+    }
+
+    handle403() {
+        this.storage.setLocalUser(null);
     }
 }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -28,7 +28,13 @@ export class ProfilePage {
         this.cliente = response;
         this.getImageIfExist();
       }, 
-      error => {});
+      error => {
+        if(error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+    } else {
+      this.navCtrl.setRoot('HomePage');
     }
 
   }
