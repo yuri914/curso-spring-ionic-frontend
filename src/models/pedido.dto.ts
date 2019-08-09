99e79605602526d3be06f2550aed02544565132a
Yuri Sousa <yuri.sousa@geq.com.br>
2019-08-09 09:19:46 -0300

    Armazenamento dos dados do pedido

---
+++ src/models/pedido.dto.ts
@@ -0,0 +1,10 @@
+import { PagamentoDTO } from "./pagamento.dto";
+import { ItemPedidoDTO } from "./item-pedido.dto";
+import { RefDTO } from "./ref.dto";
+
+export interface PedidoDTO {
+    cliente: RefDTO;
+    enderecoDeEntrega: RefDTO;
+    pagamento: PagamentoDTO;
+    itens: ItemPedidoDTO[];
+}
